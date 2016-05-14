ac345eb4095c14f0f9bbbd211cb5a99db5564a87
Luke Hansford <luke@klinify.com>
2016-05-14 14:38:45 +0800

    Add copying of config file.

--- dev-build.sh
+++ dev-build.sh
@@ -4,6 +4,7 @@ virtualenv env
 source env/bin/activate
 env/bin/pip install -r requirements.txt
 cp /home/admin/website/app.db ./app.db
+cp /home/admin/website/config.py ./config.py
 deactivate
 # Run Upstart/UWSGI
 sudo stop api-canary
